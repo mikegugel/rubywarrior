0f3bcc51731ecb74e8199c92b88388e18cfa8048
Mike Gugel <mgugel@gmail.com>
2013-05-17 13:36:21 -0400

    Complete Level 6

--- player.rb
+++ player.rb
@@ -1,23 +1,57 @@
 class Player
 
 
+  def change_dir
+    if @current_dir  == :backward
+      @current_dir = :forward
+    elsif @current_dir == :forward
+      @current_dir = :backward
+    end
+  end
+
+
+  def move(warrior)
+
+    if warrior.health >= @hp && warrior.health < 20
+      warrior.rest!
+      return
+    elsif warrior.health < @hp && warrior.health < 10
+      change_dir
+      warrior.walk! @current_dir
+      change_dir
+      return
+    end
+
+
+    if @feels[@current_dir] == "wall"
+      change_dir
+      warrior.walk! @current_dir
+    elsif @feels[@current_dir] == "Captive"
+      warrior.rescue! @current_dir
+      return
+    elsif @feels[@current_dir] == "nothing"
+      warrior.walk! @current_dir
+    else
+      warrior.attack! @current_dir
+    end
+
+  end
+
+
   def play_turn(warrior)
 
     @hp ||= 20
+    @current_dir ||= :backward
+    @feels = {
+      :forward => warrior.feel(:forward).to_s,
+      :backward => warrior.feel(:backward).to_s,
+      :left => warrior.feel(:left).to_s,
+      :right => warrior.feel(:right).to_s
+    }
 
-    if warrior.health >= @hp && warrior.health < 14 
-      warrior.rest!
-      return
-    end
+    move(warrior)
 
-
-    if warrior.feel.captive?
-      warrior.rescue!
-      return
-    end
-
-    warrior.feel.empty? ? warrior.walk! : warrior.attack!
-
+    # Save health for comparison next turn
     @hp = warrior.health
   end
 end
